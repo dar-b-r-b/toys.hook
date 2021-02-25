2432f8bdf47ff64239397d7b150da08c018c1cd5
dar-b-r-b <78546698+dar-b-r-b@users.noreply.github.com>
2021-02-25 16:01:08 +0300

    поделила код на части

--- toys.hook/server.js
+++ toys.hook/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const hbs = require('hbs');
+const { dirname } = require('path');
 const app = express();
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', function(_, resp){
-resp.send('Главная');
+app.use('/', function (_, resp) {
+    resp.render('index.hbs');
 });
 
 app.listen(3000);
