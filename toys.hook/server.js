badb7fe184a60bba8d7bcca5a610742db1d247e5
dar-b-r-b <78546698+dar-b-r-b@users.noreply.github.com>
2021-03-07 00:57:30 +0300

    фильтр в процессе

--- toys.hook/server.js
+++ toys.hook/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const hbs = require('hbs');
 const { dirname } = require('path');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.localsAsTemplateData(app);
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', function (_, resp) {
+    app.locals.styleFiles = fs.readdirSync('./public/css/').filter(x => x.endsWith('.css'));
     resp.render('index.hbs');
 });
 
